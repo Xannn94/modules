1781b58aa1f37707a5f488be88d3374619e97dc0
akhan@weltkind.com <axiles94>
2017-05-25 15:40:07 +0600

    fix admin controller

--- resources/stubs/module/Http/Controllers/Admin/IndexController.php
+++ resources/stubs/module/Http/Controllers/Admin/IndexController.php
@@ -8,6 +8,12 @@ use App\Modules\DummyName\Models\DummyName;
 
 class IndexController extends Admin
 {
+
+	 /* тут должен быть slug модуля для правильной работы меню */
+    public $page = 'DummySlug';
+    /* тут должен быть slug группы для правильной работы меню */
+    public $pageGroup = 'modules';
+	
     public function getModel(){
         return new DummyName();
     }
