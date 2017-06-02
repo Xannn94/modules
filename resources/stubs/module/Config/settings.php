cf7810ac7aee46875f270f517b9a7e12cbaa42b3
akhan@weltkind.com <axiles94>
2017-06-02 16:42:52 +0600

    add roles attribute in settings`:

--- resources/stubs/module/Config/settings.php
+++ resources/stubs/module/Config/settings.php
@@ -1,7 +1,8 @@
 <?php
 
 return[
-    'title'=>trans('DummySlug::admin.title'),
-    'localization'=>true,
+    'title'			=> trans('DummySlug::admin.title'),
+    'localization'	=> true,
+    'in_roles'		=> 1
 
 ];
